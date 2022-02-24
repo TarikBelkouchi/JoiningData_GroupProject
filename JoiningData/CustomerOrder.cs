4b07f5d35d7f255c9f07c81113c832d748bd5f69
Tarik <t.rock22@icloud.com>
2022-02-24 14:34:23 -0500

    first commit

---
+++ JoiningData/CustomerOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace JoiningData
+{
+	public class CustomerOrder
+	{
+		public string CustomerName { get; set; }
+		public string Item { get; set; }
+		public decimal Price { get; set; }
+		public int Quanity { get; set; }
+
+
+		public CustomerOrder(string _customerName, string _item, decimal _price, int _quanity)
+			{
+				CustomerName = _customerName;
+				Item = _item;
+				Price = _price;
+				Quanity = _quanity;
+			}
+
+	}
+
+	//string[] cName = { "Acme Hardware", "Acme Hardware", "Falls Realty", "Julie's Morning Diner", "Julie's Morning Diner" };
+	//string[] cItem = { "Mouse", "Keyboard", "Macbook", "iPad", "Credit Card Reader" }
+	//decimal[] cPrice = { 25.00, 45.00, 800.00, 525.00, 45.00 };
+	//int[] cQuanity = { 3, 2, 2, 1, 1 };
+
+
+
+}
+
+
+
+
+
--- JoiningData/Program.cs
+++ JoiningData/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JoiningData;
 
+CustomerOrder[] co = new CustomerOrder[]
+{
+		new CustomerOrder("Acme Hardware", "Mouse", 25.00m, 3),
+		new CustomerOrder("Acme Hardware", "Keyboard", 45.00m, 2),
+		new CustomerOrder("Falls Reality", "Macbook", 800.00m,2),
+		new CustomerOrder("Julie's Morning Diner", "iPad", 525.00m, 1),
+		new CustomerOrder("Julie's Morning Diner", "Credit Card Reader", 45.00m, 1)
+
+};
+
+string currentName = "";
+
+
+foreach(CustomerOrder c in co)
+{
+	if (currentName != c.CustomerName)
+    {
+		currentName = c.CustomerName;
+		Console.WriteLine($"{c.CustomerName}");
+		Console.WriteLine("\tItem Price Quanity Total");
+      
+        
+	}
+
+	decimal totalPrice = c.Price * c.Quanity;
+
+	
+	Console.WriteLine($"\t{c.Item} {c.Price} {c.Quanity} {totalPrice}");
+    
+}
